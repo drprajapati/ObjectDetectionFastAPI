22b8b90aeaf0bb60cf1c2707fabc82c8ce3fec7d
Darshan Prajapati <dprajapati5206@gmail.com>
2022-05-07 16:11:47 -0400

    Streamlit Support Added for client side

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
 import os
-import cv2
 import requests
-import numpy as np
-from IPython.display import Image, display
 import io
+import streamlit as st
 
 base_url = 'http://127.0.0.1:8000'
 endpoint = '/predict'
@@ -15,8 +13,12 @@ dir_name = "images_predicted"
 if not os.path.exists(dir_name):
     os.mkdir(dir_name)
 
+st.title("Object Detection using YOLOv3")
+upload = st.file_uploader("Upload an image")
+
 
 def response_from_server(url, image_file, verbose=True):
+    print(image_file)
     files = {'file': image_file}
     response = requests.post(url=url, files=files)
     status_code = response.status_code
@@ -26,18 +28,20 @@ def response_from_server(url, image_file, verbose=True):
     return response
 
 
-with open("images/apple.jpeg", "rb") as image_file:
-    prediction = response_from_server(full_url, image_file)
+if upload is not None:
+    col1, col2 = st.columns(2)
+    bool_button = st.button("Predict")
+    with col1:
+        st.header("Original Image")
+        image = upload.read()
+        st.image(image)
+        with open(os.path.join("images", upload.name), "wb") as f:
+            f.write((upload).getbuffer())
 
-
-def display_image_from_response(response):
-    image_stream = io.BytesIO(response.content)
-    image_stream.seek(0)
-    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
-    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    filename = "image_with_objects.jpeg"
-    cv2.imwrite(f'images_predicted/{filename}', image)
-    display(Image(f'images_predicted/{filename}'))
-
-
-display_image_from_response(prediction)
+    with col2:
+        st.header("Detected Object")
+        if bool_button:
+            with open(f"images/{upload.name}", "rb") as image_file:
+                prediction = response_from_server(full_url, image_file, verbose=False)
+                image_stream = io.BytesIO(prediction.content)
+                st.image(image_stream)
